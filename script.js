e3567280fbd76175226374cfbba1b851d697891e
utkarshInnoraft73 <utkarsh.singh@innoraft.com>
2024-10-03 16:07:56 +0530

    FT2-313: Fix the navbar issues to responsive.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(event){
   $('.hamburger').click(function(){
-    $('.navlinks').show('3000');
+    $('.navlinks').show('3000').focus();;
   })
   $('.accountOptions').click(function(){
-    $('.accountButton').show('3000');
+    $('.accountButton').show('3000').focus();
   })
   $('.close').click(function(){
     $('.navlinks').hide('3000');
@@ -11,4 +11,7 @@ $(document).ready(function(event){
   $('.closeButton').click(function(){
     $('.accountButton').hide('3000');
   })
+  $('.navlinks, .accountButton').on('blur', function () {
+    $(this).hide('3000');
+  });
 })
