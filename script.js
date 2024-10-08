aa90cabaaeaedb75e574ab26cd4cb16bb05a6c38
utkarshinnoraft73 <utkarsh.singh@innoraft.com>
2024-10-08 20:54:44 +0530

    FT2-315: Complete the task of SCSS.

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(event){
+    $('.hamburger').click(function(){
+      $('.nav_content__navlinks').show('3000').focus();;
+    })
+    $('.accountOptions').click(function(){
+      $('.nav_content__account_button').show('3000').focus();
+    })
+    $('.close').click(function(){
+      $('.nav_content__navlinks').hide('3000');
+    })
+    $('.closeButton').click(function(){
+      $('.nav_content__account_button').hide('3000');
+    })
+    $('.nav_content__navlinks, .nav_content__account_button').on('blur', function () {
+      $(this).hide('3000');
+    });
+  })
